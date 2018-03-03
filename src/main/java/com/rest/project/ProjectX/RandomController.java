2649542da7875420481f2f6d18367e2b2a0e13af
Kelvin Scorpio <kelvinscorpio@KS-MBP.local>
2018-03-03 15:45:41 +0000

    REFACTOR randomNum functionalities

--- src/main/java/com/rest/project/ProjectX/RandomController.java
+++ src/main/java/com/rest/project/ProjectX/RandomController.java
@@ -11,21 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class RandomController {
 	
 	
-	private static final int template = getRandomNumberWithRange(0, 100);
-    private final AtomicLong counter = new AtomicLong();
+//	private static final int template = getRandomNumberWithRange(0, 100);
+//    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/randomNum")
-    public RandomNum randomNum(@RequestParam(value="number") int number) {
-        return new RandomNum(counter.incrementAndGet(), template);
+    public int randomNum(@RequestParam(value="min", defaultValue="0") String min, 
+    		@RequestParam(value="max", defaultValue="100") String max) {
+    		int minNum = Integer.valueOf(min);
+    		int maxNum = Integer.valueOf(max);
+    		
+    		return getRandomNumberWithRange(minNum, maxNum);
+//        return new RandomNum(counter.incrementAndGet(), template);
     }
 	
 
-	private static int getRandomNumberWithRange(int min, int max) {
+	private int getRandomNumberWithRange(int min, int max) {
 		// ensure maximum is never less than minimum
-		if (min >= max) throw new IllegalArgumentException("The maximum nmaber must be greater than the min Minimum");
+		if (min >= max) 
+			throw new IllegalArgumentException("The Max number must be greater than the Min number");
 		
 		Random r = new Random();
-		int randomNumber = r.nextInt((max - min) + 1) + min;
-		return randomNumber;
+		return r.nextInt((max - min) + 1) + min;
 	}
 }
