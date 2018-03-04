e932da75582f11df07a4ee02770f09161af4c124
O'Neil Blake <oneil606@gmail.com>
2018-03-04 17:42:49 +0000

    BUG FIX input fix for none numeric char on input

--- src/main/java/com/rest/project/controllers/RandomController.java
+++ src/main/java/com/rest/project/controllers/RandomController.java
@@ -7,24 +7,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RandomController {
-	
 
-    @RequestMapping("/randomNum")
-    public int randomNum(@RequestParam(value="min", defaultValue="0") String min, 
-    		@RequestParam(value="max", defaultValue="100") String max) {
-    		int minNum = Integer.valueOf(min);
-    		int maxNum = Integer.valueOf(max);
-    		
-    		return getRandomNumberWithRange(minNum, maxNum);
-    }
-	
+	@RequestMapping("/randomNum")
+	public int randomNum(@RequestParam(value = "min", defaultValue = "0") String min,
+			@RequestParam(value = "max", defaultValue = "100") String max) {
+
+		int minNum=0;
+		int maxNum=0;
+		
+		if (isInteger(min) == false || isInteger(max) == false) {
+			throw new IllegalArgumentException("Please ensure you enter a numeric value for both Minimum and Maximum");
+		} else {
+			 minNum = Integer.parseInt(min);
+			 maxNum = Integer.parseInt(max);
+		}
+		return getRandomNumberWithRange(minNum, maxNum);
+	}
 
 	private int getRandomNumberWithRange(int min, int max) {
 		// ensure maximum is never less than minimum
-		if (min >= max) 
+		if (min >= max)
 			throw new IllegalArgumentException("The Max number must be greater than the Min number");
-		
+
 		Random r = new Random();
 		return r.nextInt((max - min) + 1) + min;
+	}
+
+	//test if a string can be parsed as an integer
+	public boolean isInteger(String stringToTest) {
+		try {
+			Integer.valueOf(stringToTest);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 }
